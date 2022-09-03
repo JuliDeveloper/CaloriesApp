67b0c152bb0f7bb7b6354e51903d0694bb7a71e3
JuliDeveloper <rja-97@yandex.ru>
2022-09-03 19:00:50 +0300

    Добавила кастомные цвета для проекта

---
+++ CaloriesApp/Resources/CustomColors.swift
@@ -0,0 +1,15 @@
+//
+//  CustomColors.swift
+//  CaloriesApp
+//
+//  Created by Julia Romanenko on 03.09.2022.
+//
+
+import UIKit
+
+struct CustomColors {
+    static let bacgroundColor = UIColor(red: 251/255, green: 249/255, blue: 238/255, alpha: 1)
+    static let lightGreen = UIColor(red: 246/255, green: 242/255, blue: 213/255, alpha: 1)
+    static let middleGreen = UIColor(red: 237/255, green: 230/255, blue: 176/255, alpha: 1)
+    static let darkGreen = UIColor(red: 231/255, green: 221/255, blue: 149/255, alpha: 1)
+}
