45e2c5d51212004c0b426ab255141d28406d99f8
JuliDeveloper <rja-97@yandex.ru>
2022-09-03 18:51:17 +0300

    Добавила новый контроллер DetailsFoodViewController

---
+++ CaloriesApp/ViewControllers/DetailsFoodViewController.swift
@@ -0,0 +1,17 @@
+//
+//  DetailsFoodViewController.swift
+//  CaloriesApp
+//
+//  Created by Julia Romanenko on 03.09.2022.
+//
+
+import UIKit
+
+class DetailsFoodViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+    }
+    
+}
