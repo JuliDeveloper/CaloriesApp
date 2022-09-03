f8f8081f40b5910de9f7bb86834e388583639348
JuliDeveloper <rja-97@yandex.ru>
2022-09-03 19:28:25 +0300

    Настроила внешний вид и добавила кнопки для navigation bar в FoodListTVC; изменила кастомные цвета

--- CaloriesApp/Resources/CustomColors.swift
+++ CaloriesApp/Resources/CustomColors.swift
@@ -8,8 +8,8 @@
 import UIKit
 
 struct CustomColors {
-    static let bacgroundColor = UIColor(red: 251/255, green: 249/255, blue: 238/255, alpha: 1)
+    static let backgroundColor = UIColor(red: 251/255, green: 249/255, blue: 238/255, alpha: 1)
     static let lightGreen = UIColor(red: 246/255, green: 242/255, blue: 213/255, alpha: 1)
     static let middleGreen = UIColor(red: 237/255, green: 230/255, blue: 176/255, alpha: 1)
-    static let darkGreen = UIColor(red: 231/255, green: 221/255, blue: 149/255, alpha: 1)
+    static let darkGreen = UIColor(red: 188/255, green: 194/255, blue: 78/255, alpha: 1)
 }
--- CaloriesApp/ViewControllers/FoodListTableViewController.swift
+++ CaloriesApp/ViewControllers/FoodListTableViewController.swift
@@ -13,8 +13,8 @@ final class FoodListTableViewController: UITableViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        tableView.backgroundColor = .orange
-        tableView.register(UITableViewCell.self, forCellReuseIdentifier: reuseIdentifier)
+        configTableView()
+        setupButtonsNavBar()
     }
 }
 
@@ -36,11 +36,36 @@ extension FoodListTableViewController {
 // MARK: - Table view data source
 extension FoodListTableViewController {
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        <#code#>
+        
     }
 }
 
 // MARK: - Private functions
 extension FoodListTableViewController {
+    private func configTableView() {
+        title = "CaloriesApp"
+        navigationController?.navigationBar.barTintColor = CustomColors.lightGreen
+        navigationController?.navigationBar.prefersLargeTitles = true
+        navigationController?.navigationBar.largeTitleTextAttributes = [
+            .foregroundColor: CustomColors.darkGreen
+        ]
+        
+        view.backgroundColor = CustomColors.backgroundColor
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: reuseIdentifier)
+        
+        tableView.separatorColor = CustomColors.darkGreen
+    }
     
+    private func setupButtonsNavBar() {
+        let addButton = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(addNewFood))
+        addButton.tintColor = CustomColors.darkGreen
+        navigationController?.navigationBar.topItem?.rightBarButtonItem = addButton
+        
+        navigationItem.leftBarButtonItem = self.editButtonItem
+        navigationItem.leftBarButtonItem?.tintColor = CustomColors.darkGreen
+    }
+    
+    @objc private func addNewFood() {
+        
+    }
 }
